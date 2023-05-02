4d5cc25e8b4a756b0230ea6eb98d0f51a12b0785
DavidRose <DavidRose331@Gmail.com>
2023-05-01 20:35:13 -0500

    initial

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }
+            else if (op == "/") 
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if(op == "*")
+            {
+                Console.WriteLine( num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+
+
+
+
+}
